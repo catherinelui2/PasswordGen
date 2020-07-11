047d1d68e7e5ceff6fad1bc1b9486e6f6cdd65a1
SpazCat <catherinelui.2@gmail.com>
2020-07-10 21:13:01 -0600

    Added functions to return symbols and added all the prompts for users to choose character length, lowercase, uppercase, numbers, and symbols

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random()*10)+48);
 }
 
+function getRandomSymbol() {
+  var symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols[0];
+}
+
+
+//prompt users to choose
+var charHowMany = prompt("How many characters would you like your pw to be?");
+var charLower = prompt("Would you like to include lowercase letters?");
+var charUpper = prompt("Would you like to include uppercase letters? ");
+var charSymbols = prompt("Would you like to include special characters?");
+
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
