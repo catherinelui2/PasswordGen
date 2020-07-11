32338b1351e2dc7423d34c4cf13bfffba8230840
CatL <catherinelui.2@gmail.com>
2020-07-11 12:37:30 -0600

    Added psuedo codes and if then statements

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 
 //prompt users to choose
-var charHowMany = prompt ("How many characters would you like your pw to be?");
+var userInput = prompt ("How many characters would you like your pw to be?");
 var charLower = confirm("Would you like to include lowercase letters?");
 var charUpper = confirm("Would you like to include uppercase letters? ");
 var charSymbols = confirm("Would you like to include special characters?");
@@ -25,16 +25,35 @@ function getRandomSymbols() {
   return symbols[0];
 }
 
+//Need to genearte pw based on selection
+//If then statements to filter through confirm selection
+if (charLower){
+  return getRandomLower;
+}
+else if (charUpper){
+  return getRandomUpper;
+}
+else if (charSymbols){
+  return getRandomSymbols;
+}
 
+// Use Userinput # characters to determine the length of the password
+function pwLength () {
+  
+}
+
+function generatePassword (){
+
+  
+}
+// generate the pw into the input box
 var generateBtn = document.querySelector("#generate");
-
 
 // // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  generatePassword += 
+  var generatePassword += passwordText;
 
   
   passwordText.value = password;
