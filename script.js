51a36886e57ff324070f37cd55c73d617d57f614
CatL <catherinelui.2@gmail.com>
2020-07-11 15:57:51 -0600

    Updated a if statement to alert users to choose character 8-128 range

--- script.js
+++ script.js
@@ -2,28 +2,32 @@
 
 //prompt users to choose
 var userInput = prompt("How many characters would you like your pw to be?");
-var charLower = confirm("Would you like to include lowercase letters?");
-var charUpper = confirm("Would you like to include uppercase letters?");
-var charSymbols = confirm("Would you like to include special characters?");
 var symbols = '!@#$%^&*(){}[]=<>/,.';
 var numbers = '0123456789'
 var lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
-var upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVW'
+var upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 var selected = '';
-
 
 //Need to genearte pw based on selection
 
 //If then statements to filter through confirm selection
+if ((userInput <=8) || (userInput >=128)){
+  alert("Please select a minimum of 8 and a maximum of 128 characters!")
+}
+//Prompt these after the alert, if user did not select the 8-128 range
+var charLower = confirm("Would you like to include lowercase letters?");
+var charUpper = confirm("Would you like to include uppercase letters?");
+var charSymbols = confirm("Would you like to include special characters?");
+
 if (charLower){ 
 //add string charLower to total characterCount
- selected = selected + lowerLetters;
+  selected = selected + lowerLetters;
 }
 if (charUpper){
- selected = selected + upperLetters
+  selected = selected + upperLetters;
 }
 if (charSymbols){
-selected = selected + symbols
+  selected = selected + symbols;
 }
 
 
@@ -34,7 +38,7 @@ var generateBtn = document.querySelector("#generate");
 // // Write password to the #password input
 function writePassword() {
   var passwordResult = '';
-	for (var i =0; i < userInput; i++); {
+	for (var i =0; i <= userInput; i++) {
 passwordResult = passwordResult + selected[Math.floor(Math.random()*selected.length)];
 }
 
