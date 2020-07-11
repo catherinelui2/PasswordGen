eca6819ae22d057ce5b7c7acdf79e38caa233bcb
SpazCat <catherinelui.2@gmail.com>
2020-07-10 23:26:51 -0600

    cleaned it up more to make the confirm and prompt to work

--- script.js
+++ script.js
@@ -1,58 +1,43 @@
 // Assignment Code
 
-//DOM elemnts
-var resultsEl = document.getElementById("password");
-var lengthEl = document.getElementById("length");
-var uppercaseEl = document.getElementById("uppercase");
-var lowercaseEl = document.getElementById("lowercase");
-var numbersEl = document.getElementById("numbers");
-var symbolsEl = document.getElementById("symbols");
-var generateEl = document.getElementById("generate");
-
-
-var randomPw = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumbers,
-  symbols: getRandomSymbols,
-};
+//prompt users to choose
+var charHowMany = prompt ("How many characters would you like your pw to be?");
+var charLower = confirm("Would you like to include lowercase letters?");
+var charUpper = confirm("Would you like to include uppercase letters? ");
+var charSymbols = confirm("Would you like to include special characters?");
 
 //Generate functions for uppercase, lowercase, numbers and symbols
 
 function getRandomLower() {
-@@ -10,30 +27,43 @@ function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+
+function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random()*26)+65);
 }
 
-function getRandomNumber() {
 function getRandomNumbers() {
   return String.fromCharCode(Math.floor(Math.random()*10)+48);
 }
 
-function getRandomSymbol() {
 function getRandomSymbols() {
   var symbols = '!@#$%^&*(){}[]=<>/,.';
   return symbols[0];
 }
 
 
-//prompt users to choose
-var charHowMany = prompt("How many characters would you like your pw to be?");
-var charLower = prompt("Would you like to include lowercase letters?");
-var charUpper = prompt("Would you like to include uppercase letters? ");
-var charSymbols = prompt("Would you like to include special characters?");
-
-
-  // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+// // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  generatePassword += 
 
+  
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
