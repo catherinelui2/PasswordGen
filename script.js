8f43fb0ba5e3d281e843e186b05d5bbcf5418180
SpazCat <catherinelui.2@gmail.com>
2020-07-10 22:17:48 -0600

    Added check boxes into HTLM, added DOM elements , figuring out how to call and filter check boxes

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Assignment Code
 
+//DOM elemnts
+var resultsEl = document.getElementById("password");
+var lengthEl = document.getElementById("length");
+var uppercaseEl = document.getElementById("uppercase");
+var lowercaseEl = document.getElementById("lowercase");
+var numbersEl = document.getElementById("numbers");
+var symbolsEl = document.getElementById("symbols");
+var generateEl = document.getElementById("generate");
+
+
+var randomPw = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumbers,
+  symbols: getRandomSymbols,
+};
+
 //Generate functions for uppercase, lowercase, numbers and symbols
 
 function getRandomLower() {
@@ -10,30 +27,43 @@ function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random()*26)+65);
 }
 
-function getRandomNumber() {
+function getRandomNumbers() {
   return String.fromCharCode(Math.floor(Math.random()*10)+48);
 }
 
-function getRandomSymbol() {
+function getRandomSymbols() {
   var symbols = '!@#$%^&*(){}[]=<>/,.';
   return symbols[0];
 }
 
 
-//prompt users to choose
-var charHowMany = prompt("How many characters would you like your pw to be?");
-var charLower = prompt("Would you like to include lowercase letters?");
-var charUpper = prompt("Would you like to include uppercase letters? ");
-var charSymbols = prompt("Would you like to include special characters?");
 
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
   
-  
-  passwordText.value = password;
+  var length = +lengthEl.value;
+  var isLower = lowercaseEl.checked;
+  var isUpper = uppercaseEl.checked;
+  var isNumber = numbersEl.checked;
+  var isSymbol = symbolsEl.checked;
+
+  resultsEl.innerText = generatePassword(isLower,isUpper, isNumber, isSymbol);
+
+}
+
+function generatePassword (lower, upper, number, symbols,length) {
+// start pw var
+//filter the unchecked
+//loop over lengh of call generator function 
+//add final pw and return
+
+var generatedPassword = "";
+
+var typesCount = lower + upper + number + symbols;
+
+var typesArr = [{lower}, {upper}, {number}, {symbols}].filter(item => Object.values(item)[0]);
+
 
 }
 
